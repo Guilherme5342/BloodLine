a02aed420abcf534475375142597c947c5736900
Caio IESB Hudson UnB <caiohudson.red4@hotmail.com>
2023-10-27 15:41:55 -0300

    Referencias pra Background

---
+++ assets/map/03 water.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="03 water" tilewidth="32" tileheight="32" tilecount="20" columns="4">
+ <image source="imageRefs/03 water.png" width="128" height="160"/>
+</tileset>
