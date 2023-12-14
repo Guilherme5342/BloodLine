82f95622317ed6e28c885394be587004d1de0134
McKinley <caiohudson.red4@hotmail.com>
2023-12-14 19:20:10 -0300

    Color e GlobalDefinitions adicionados
    - Estes incluem todos os defines e strings que estavam na Engine passada, do player e do inimigo para as 2 plataformas
    - Precisa importar novamente as pastas do Player

---
+++ Bloodline/src/GameDefines/ColorDefinitions.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+
+struct Color {
+
+public:
+	static const SDL_Color Red;//{255,0,0,255};
+	static const SDL_Color Magenta;//{ 255,0,255,255 };
+	static const SDL_Color Blue;//{ 0,0,255,255 };
+	static const SDL_Color Green;//{ 0,255,0,255 };
+	static const SDL_Color Yellow;//{ 255,255,0,255 };
+
+
+	static const SDL_Color White;//{ 255,255,255,255 };
+	static const SDL_Color Black;//{ 0,0,0,255 };
+
+
+};
---
+++ Bloodline/src/GameDefines/GlobalDefinitions.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#ifdef _WIN32
+#define BALL_PNG "assets/img/ball2.png"
+#define STUB_ANIMATED_SPRITE "assets/img/packedSpriteSheet1.png"s
+
+#define PLAYER_IDLE "assets/img/player/idle.png"
+#define PLAYER_JUMP "assets/img/player/jump.png"
+#define PLAYER_RUNNING "assets/img/player/run.png"
+#define PLAYER_ATTACK "assets/img/player/jump.png"
+
+
+#define PAWN_ENEMY_IDLE "assets/img/enemies/knight/_Idle.png"
+#define PAWN_ENEMY_MOVE "assets/img/enemies/knight/_Run.png"
+#define PAWN_ENEMY_ATTACK "assets/img/enemies/knight/_Attack.png"
+#define PAWN_ENEMY_SPECIAL_ATTACK "assets/img/enemies/knight/_AttackCombo.png"
+
+
+/*TileSets FilePaths*/
+#define TILESET_WATER_STUB "assets/map/imageRefs/03 water.png"
+#define TILESET_ICE_STUB "assets/img/tileset.png"
+
+/*TileMap FilePaths*/
+#define MAP_STUB "assets/map/mapaStub2.csv"
+
+#else
+#define BALL_PNG "../assets/img/ball2.png"
+#define STUB_ANIMATED_SPRITE "../assets/img/packedSpriteSheet1.png"
+
+#define PLAYER_IDLE "../assets/img/player/idle.png"
+#define PLAYER_JUMP "../assets/img/player/jump.png"
+#define PLAYER_RUNNING "../assets/img/player/run.png"
+#define PLAYER_ATTACK "../assets/img/player/jump.png"
+
+
+#define PAWN_ENEMY_IDLE "../assets/img/enemies/knight/_Idle.png"
+#define PAWN_ENEMY_MOVE "../assets/img/enemies/knight/_Run.png"
+#define PAWN_ENEMY_ATTACK "../assets/img/enemies/knight/_Attack.png"
+#define PAWN_ENEMY_SPECIAL_ATTACK "../assets/img/enemies/knight/_AttackCombo.png"
+
+#define TILESET_WATER_STUB "../assets/map/imageRefs/03 water.png"
+#define TILESET_ICE_STUB "../assets/img/tileset.png"
+
+#define MAP_STUB "../assets/map/mapaStub2.csv"
+#endif
+
+enum Action {
+	IDLE = 0,
+	MOVE = 1,
+	ATTACK = 2,
+	MAGIC_SPELL = 3
+};
+
+enum EnemyTypePhysics { // A Física que define os tipos de inimigos
+	GROUND = (1),   
+	FLY = (1,0),
+};
