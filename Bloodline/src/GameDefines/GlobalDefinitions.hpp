8274a293de51fc3762edcf0f499634542a77d832
McKinley <caiohudson.red4@hotmail.com>
2023-12-14 20:01:29 -0300

    trocando pra res

--- Bloodline/src/GameDefines/GlobalDefinitions.hpp
+++ Bloodline/src/GameDefines/GlobalDefinitions.hpp
@@ -1,47 +1,47 @@
 #pragma once
 
 #ifdef _WIN32
-#define BALL_PNG "assets/img/ball2.png"
-#define STUB_ANIMATED_SPRITE "assets/img/packedSpriteSheet1.png"s
+#define BALL_PNG "res/img/ball2.png"
+#define STUB_ANIMATED_SPRITE "res/img/packedSpriteSheet1.png"s
 
-#define PLAYER_IDLE "assets/img/player/idle.png"
-#define PLAYER_JUMP "assets/img/player/jump.png"
-#define PLAYER_RUNNING "assets/img/player/run.png"
-#define PLAYER_ATTACK "assets/img/player/jump.png"
+#define PLAYER_IDLE "res/img/player/idle.png"
+#define PLAYER_JUMP "res/img/player/jump.png"
+#define PLAYER_RUNNING "res/img/player/run.png"
+#define PLAYER_ATTACK "res/img/player/jump.png"
 
 
-#define PAWN_ENEMY_IDLE "assets/img/enemies/knight/_Idle.png"
-#define PAWN_ENEMY_MOVE "assets/img/enemies/knight/_Run.png"
-#define PAWN_ENEMY_ATTACK "assets/img/enemies/knight/_Attack.png"
-#define PAWN_ENEMY_SPECIAL_ATTACK "assets/img/enemies/knight/_AttackCombo.png"
+#define PAWN_ENEMY_IDLE "res/img/enemies/knight/_Idle.png"
+#define PAWN_ENEMY_MOVE "res/img/enemies/knight/_Run.png"
+#define PAWN_ENEMY_ATTACK "res/img/enemies/knight/_Attack.png"
+#define PAWN_ENEMY_SPECIAL_ATTACK "res/img/enemies/knight/_AttackCombo.png"
 
 
 /*TileSets FilePaths*/
-#define TILESET_WATER_STUB "assets/map/imageRefs/03 water.png"
-#define TILESET_ICE_STUB "assets/img/tileset.png"
+#define TILESET_WATER_STUB "res/map/imageRefs/03 water.png"
+#define TILESET_ICE_STUB "res/img/tileset.png"
 
 /*TileMap FilePaths*/
-#define MAP_STUB "assets/map/mapaStub2.csv"
+#define MAP_STUB "res/map/mapaStub2.csv"
 
 #else
-#define BALL_PNG "../assets/img/ball2.png"
-#define STUB_ANIMATED_SPRITE "../assets/img/packedSpriteSheet1.png"
+#define BALL_PNG "../res/img/ball2.png"
+#define STUB_ANIMATED_SPRITE "../res/img/packedSpriteSheet1.png"
 
-#define PLAYER_IDLE "../assets/img/player/idle.png"
-#define PLAYER_JUMP "../assets/img/player/jump.png"
-#define PLAYER_RUNNING "../assets/img/player/run.png"
-#define PLAYER_ATTACK "../assets/img/player/jump.png"
+#define PLAYER_IDLE "../res/img/player/idle.png"
+#define PLAYER_JUMP "../res/img/player/jump.png"
+#define PLAYER_RUNNING "../res/img/player/run.png"
+#define PLAYER_ATTACK "../res/img/player/jump.png"
 
 
-#define PAWN_ENEMY_IDLE "../assets/img/enemies/knight/_Idle.png"
-#define PAWN_ENEMY_MOVE "../assets/img/enemies/knight/_Run.png"
-#define PAWN_ENEMY_ATTACK "../assets/img/enemies/knight/_Attack.png"
-#define PAWN_ENEMY_SPECIAL_ATTACK "../assets/img/enemies/knight/_AttackCombo.png"
+#define PAWN_ENEMY_IDLE "../res/img/enemies/knight/_Idle.png"
+#define PAWN_ENEMY_MOVE "../res/img/enemies/knight/_Run.png"
+#define PAWN_ENEMY_ATTACK "../res/img/enemies/knight/_Attack.png"
+#define PAWN_ENEMY_SPECIAL_ATTACK "../res/img/enemies/knight/_AttackCombo.png"
 
-#define TILESET_WATER_STUB "../assets/map/imageRefs/03 water.png"
-#define TILESET_ICE_STUB "../assets/img/tileset.png"
+#define TILESET_WATER_STUB "../res/map/imageRefs/03 water.png"
+#define TILESET_ICE_STUB "../res/img/tileset.png"
 
-#define MAP_STUB "../assets/map/mapaStub2.csv"
+#define MAP_STUB "../res/map/mapaStub2.csv"
 #endif
 
 enum Action {
